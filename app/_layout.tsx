db363ed2b3b6355692168966bf934851751c4475
lewisggp <lewisggp@users.noreply.github.com>
2024-09-11 11:05:27 -0400

    fix fonts loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,32 @@
-import { useFonts } from "expo-font";
+import { useState, useEffect } from "react";
+import * as Font from "expo-font";
 import { Stack } from "expo-router";
+import { View, Text, ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
-  useFonts({
-    'outfit-regular': require('@/assets/fonts/Outfit-Regular.ttf'),
-    'outfit-medium': require('@/assets/fonts/Outfit-Medium.ttf'),
-    'outfit-bold': require('@/assets/fonts/Outfit-Bold.ttf'),
-  })
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'outfit-regular': require('@/assets/fonts/Outfit-Regular.ttf'),
+        'outfit-medium': require('@/assets/fonts/Outfit-Medium.ttf'),
+        'outfit-bold': require('@/assets/fonts/Outfit-Bold.ttf'),
+      });
+      setFontsLoaded(true);
+    }
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <Stack>
